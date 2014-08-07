b12d7179413eb6ee06cf884fa75ea4b288a480a8
Mitch VanDuyn <mitch@catprint.com>
2014-08-07 18:37:23 +0000

    separate templates for each function in nav

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,11 +2,19 @@ import Ember from 'ember';
 
 var ApplicationController = Ember.Controller.extend({
   init: function() {
-    this.set('selected', 'Tutorial');
+    this.set('content', Ember.A([
+      Ember.Object.create({title: 'Tutorial', template: 'tutorial', disabled: false}),
+      Ember.Object.create({title: 'Select Your Design', template: 'select', disabled: false, enables: 'Personalize'}),
+      Ember.Object.create({title: 'Personalize', template: 'personalize', disabled: true, enables: 'Place Order'}),
+      Ember.Object.create({title: 'Place Order', template: 'order', disabled: true}),
+    ]));
+    this.set('selected', this.get('content').objectAt(0));
   },
-  content: ['Tutorial', 'Select Your Design', 'Personalize', 'Order'],
   contentChanged: (function() {
-    return console.log("Selection has changed to: " + (this.get('selected')));
+    if (this.get('selected.enables')) {
+      this.get('content').findBy('title', this.get('selected.enables')).set('disabled', false);
+    }
+    return console.log("Selection has changed to: " + (this.get('selected.title')));
   }).observes('selected')
 });
 
